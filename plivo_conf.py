8075a06d980c8ea7102a3b0a0e44a2f0e19d7674
Mike Evans <mike@metaswitch.com>
2013-08-13 07:30:53 +0100

    Code review updates

--- plivo_conf.py
+++ plivo_conf.py
@@ -1,8 +1,45 @@
+#!/usr/bin/env python
+
+# @file plivo_conf.py
+#
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2013  Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
+import os
 from flask import Flask, request, make_response
 import plivo
-import os
 
-SERVER_NAME = '127.0.0.1:5000'
+SERVER_PORT = 5000
+SERVER_NAME = '127.0.0.1:%s' % SERVER_PORT
 
 app = Flask(__name__)
 
@@ -10,13 +47,13 @@ app = Flask(__name__)
 def confselect():
     response = plivo.Response()
     getdigits = response.addGetDigits(
-        action='http://' + SERVER_NAME + '/confroom/',
+        action='http://%s/confroom/' % SERVER_NAME,
         timeout='15',
         finishOnKey='#',
         method='GET'
     )
     getdigits.addWait(length='1')
-    getdigits.addSpeak('Please enter your conference i d, followed by the pound key.')
+    getdigits.addSpeak("Please enter your conference i d, followed by the pound key.")
     response.addSpeak(body="Input not received. Thank you.")
     xml_response = make_response(response.to_xml())
     xml_response.headers["Content-type"] = "text/xml"
@@ -30,11 +67,11 @@ def confroom():
         conf = request.form.get('Digits', '')
 
     response = plivo.Response()
-    response.addSpeak(body="Now entering conference " + ",".join(list(str(conf))))
+    response.addSpeak(body="Now entering conference " + ",".join(str(conf)))
     response.addConference(
         enterSound='beep:2',
         exitSound='beep:1',
-        waitSound='http://' + SERVER_NAME + '/confwait/',
+        waitSound='http://%s/confwait/' % SERVER_NAME,
         body=conf
     )
 
@@ -46,9 +83,7 @@ def confroom():
 @app.route('/confwait/', methods=['POST'])
 def confwait():
     response = plivo.Response()
-    # response.addSpeak(body='You are the first participant')
     response.addPlay(body='http://localhost/music.mp3')
-    # response.addWait(length='10')
 
     print response.to_xml()
     xml_response = make_response(response.to_xml())
@@ -56,5 +91,4 @@ def confwait():
     return xml_response
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=SERVER_PORT)
--- plivo_vm.py
+++ plivo_vm.py
@@ -1,9 +1,46 @@
-from flask import Flask, request, make_response
-import plivo
+#!/usr/bin/env python
+
+# @file plivo_vm.py
+#
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2013  Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
 import os
 import time
+from flask import Flask, request, make_response
+import plivo
 
-SERVER_NAME = '127.0.0.1:5000'
+SERVER_PORT = 5000
+SERVER_NAME = '127.0.0.1:%s' % SERVER_PORT
 MAILBOX_ROOT = '/home/ubuntu/plivo-vm/mailboxes'
 RECORD_FORMAT = 'mp3'
 
@@ -12,12 +49,12 @@ app = Flask(__name__)
 class Message(object):
     @staticmethod
     def load(msg_file, directory):
-        msg_key = msg_file[:-(1+len(RECORD_FORMAT))]
+        msg_key = os.path.splitext(msg_file)[0]
         return Message(msg_key, directory)
 
     def __init__(self, msg_key, directory):
         self.msg_key = msg_key
-        self.file = os.path.join(directory, msg_key + '.' + RECORD_FORMAT)
+        self.directory = directory
         self.received = int(msg_key.split('_')[0])
         self.from_number = msg_key.split('_')[1]
 
@@ -28,32 +65,34 @@ class Message(object):
             print 'Move failed'
             raise e
 
-    def set_directory(self, directory):
-        self.file = os.path.join(directory, self.msg_key + '.' + RECORD_FORMAT)
+    def file ():
+        return os.path.join(directory, '%s.%s' % (self.msg_key, RECORD_FORMAT))
 
-    def __str__(self):
-        return self.msg_key
+    #def __str__(self):
+    #    return self.msg_key
 
-    def __repr__(self):
-        return self.msg_key
+    #def __repr__(self):
+    #    return self.msg_key
 
-    nth = ['zero',
-           'first','second','third','fourth','fifth','sixth','seventh','eighth','ninth',
-           'tenth','eleventh','twelth','thirteenth','fourteenth','fifteenth','sixteenth','seventeenth','eighteenth','nineteenth',
-           'twentieth', 'twenty-first','twenty-second','twenty-third','twenty-fourth','twenty-fifth','twenty-sixth','twenty-seventh','twenty-eighth','twenty-ninth',
-           'thirtieth','thirty-first']
+    osuffix = {'1':"st",'2':"nd",'3':"rd"}
+
+    def ordinalize(number):
+        s = str(number)
+        if len(s) == 1 or s[-2] != '1':
+            return s + osuffix.get(s[-1],"th")
+        else:
+            return s + "th"
 
     def description(self):
         # Create a textual description of the message based on the file name
         received = time.localtime(self.received)
-        desc = 'Message received at ' + time.strftime('%I,%M,%p', received)
+        time_received = time.strftime("%I,%M,%p", received)
         now = time.localtime()
         if received.tm_year == now.tm_year and received.tm_yday == now.tm_yday:
-            desc += ' today'
+            date_received = "today"
         else:
-            desc += ' on ' + time.strftime('%A,%B', received) + ' ' + Message.nth[received.tm_mday]
-        desc += ' from ' + ",".join(list(self.from_number))
-        return desc
+            date_received = "on %s %s" % (time.strftime("%A,%B", received), Message.ordinalize(received.tm_mday))
+        return "Message received at %s %s from %s" % (time_received, date_received, ",".join(self.from_number))
 
 class Mailbox(object):
     def __init__(self, mailbox_id):
@@ -74,9 +113,9 @@ class Mailbox(object):
         # Populate message lists, sorted oldest first.
         self.new_msgs = [Message.load(x, self.new_msg_directory) for x in os.listdir(self.new_msg_directory)]
         self.saved_msgs = [Message.load(x, self.saved_msg_directory) for x in os.listdir(self.saved_msg_directory)]
-        self.new_msgs.sort(key = lambda x: x.msg_key)
-        self.saved_msgs.sort(key = lambda x: x.msg_key)
-        print 'Retrieved mailbox %s, %d new messages, %d saved messages' % (mailbox_id, len(self.new_msgs), len(self.saved_msgs))
+        self.new_msgs.sort(key = lambda x: x.received)
+        self.saved_msgs.sort(key = lambda x: x.received)
+        print "Retrieved mailbox %s, %d new messages, %d saved messages" % (mailbox_id, len(self.new_msgs), len(self.saved_msgs))
 
     def id(self):
         return self.mailbox_id
@@ -96,14 +135,10 @@ class Mailbox(object):
             self.new_msgs.remove(msg)
 
     def message_read(self, msg):
-        print 'Mark message ' + msg.msg_key + ' as read'
         if msg in self.new_msgs:
-            print 'Found new message, so move to saved messages'
             new_msg_file = msg.file
-            msg.set_directory(self.saved_msg_directory)
-            print 'Move from ' + new_msg_file + ' to ' + msg.file
+            msg.directory = self.saved_msg_directory
             os.rename(new_msg_file, msg.file)
-            print 'Move succeeded'
             self.new_msgs.remove(msg)
             self.saved_msgs.append(msg)
             self.saved_msgs.sort(key=lambda x: x.received)
@@ -111,51 +146,51 @@ class Mailbox(object):
     def message_unread(self, msg):
         if msg in self.saved_msgs:
             saved_msg_file = msg.file
-            msg.set_directory(self.new_msg_directory)
+            msg.directory = self.new_msg_directory
             os.rename(saved_msg_file, msg.file)
             self.saved_msgs.remove(msg)
             self.new_msgs.append(msg)
             self.new_msgs.sort(key=lambda x: x.received)
 
     def set_greeting(self, rec_file):
-        greeting = os.path.join(self.root, 'greeting.' + RECORD_FORMAT)
+        greeting = os.path.join(self.root, "greeting.%s" % RECORD_FORMAT)
         try:
             os.rename(rec_file, greeting)
         except OSError as e:
-            print 'Move failed'
+            print "Move failed"
             raise e
 
     def get_greeting(self):
-        greeting = os.path.join(self.root, 'greeting.' + RECORD_FORMAT)
+        greeting = os.path.join(self.root, "greeting.%s" % RECORD_FORMAT)
         if os.path.exists(greeting):
             return greeting
         else:
             return ''
 
-def play_message(response, mailbox, context, msg):
+def play_message(response, mailbox, list, time, msg):
     mailbox.message_read(msg);
     getdigits = response.addGetDigits(
-        action='http://' + SERVER_NAME + '/vmmsgoption/' + mailbox.id() + '/' + context + '/' + msg.msg_key,
+        action="http://" + SERVER_NAME + "/vmmsgoption/" + mailbox.id() + "/" + list + "/" + time + "/" + msg.msg_key,
         timeout='60',
         numDigits='1',
         method='GET'
     )
     getdigits.addSpeak(body=msg.description())
     getdigits.addPlay(msg.file)
-    getdigits.addSpeak(body='Press 2 to repeat, 3 to delete, 6 to skip, 7 to save')
+    getdigits.addSpeak(body="Press 2 to repeat, 3 to delete, 6 to skip, 7 to save")
 
 def play_menu(response, mailbox):
-    response.addSpeak(body='Main menu')
+    response.addSpeak(body="Main menu")
     getdigits = response.addGetDigits(
-        action='http://' + SERVER_NAME + '/vmmenu/' + mailbox.id(),
+        action="http://" + SERVER_NAME + "/vmmenu/" + mailbox.id(),
         timeout='15',
         numDigits='1',
         method='GET'
     )
-    getdigits.addSpeak(body='Press 2 for your messages, 3 to record a personal greeting')
-    response.addSpeak(body='No option selected')
+    getdigits.addSpeak(body="Press 2 for your messages, 3 to record a personal greeting")
+    response.addSpeak(body="No option selected")
 
-@app.route('/answered/', methods=['GET'])
+@app.route("/answered/", methods=['GET'])
 def vm():
     forward_num = request.args.get('ForwardedFrom', '')
     from_num = request.args.get('From', '')
@@ -170,35 +205,31 @@ def vm():
         if greeting != '':
             response.addPlay(body=greeting)
         else:
-            response.addSpeak(body='You have reached the voicemail of ' + ",".join(list(mailbox.id())) + '.  Please leave a message after the beep.')
+            response.addSpeak(body="You have reached the voicemail of %s.  Please leave a message after the beep." % ",".join(mailbox.id()))
         response.addRecord(
-              action='http://' + SERVER_NAME + '/vmdeposit/' + mailbox.id() + '/' + from_num + '/' + str(int(time.time())),
+              action="http://" + SERVER_NAME + "/vmdeposit/" + mailbox.id() + "/" + from_num + "/" + str(int(time.time())),
               fileFormat=RECORD_FORMAT,
               finishOnKey='#',
               method='GET')
-        response.addSpeak(body='Message not received')
+        response.addSpeak(body="Message not received")
     elif from_num != '':
         # Call is to a mailbox, so play the message status and any messages.
         mailbox = Mailbox(from_num)
-        response.addSpeak(body='%d new messages and %d saved messages' % (len(mailbox.new_msgs), len(mailbox.saved_msgs)))
+        response.addSpeak(body="%d new messages and %d saved messages" % (len(mailbox.new_msgs), len(mailbox.saved_msgs)))
         if len(mailbox.new_msgs) > 0:
             msg = mailbox.new_msgs[0]
-            # Set the context to be the character n plus the time of the first
-            # new message in the list.  We use this time to make sure we don't
-            # play new messages twice in this pass.
-            context = 'n' + str(msg.received)
-            play_message(response, mailbox, context, msg)
+            play_message(response, mailbox, 'new', str(msg.received), msg)
         else:
             play_menu(response, mailbox)
     else:
-        response.addSpeak(body='Cannot find mailbox')
+        response.addSpeak(body="Cannot find mailbox")
 
     print response.to_xml()
     xml_response = make_response(response.to_xml())
     xml_response.headers["Content-type"] = "text/xml"
     return xml_response
 
-@app.route('/vmdeposit/<id>/<from_num>/<received>', methods=['GET'])
+@app.route("/vmdeposit/<id>/<from_num>/<received>", methods=['GET'])
 def vmdeposit(id, from_num, received):
     rec_file = request.args.get('RecordFile', '')
 
@@ -218,8 +249,8 @@ def vmdeposit(id, from_num, received):
     xml_response.headers["Content-type"] = "text/xml"
     return xml_response
 
-@app.route('/vmmsgoption/<id>/<context>/<msg_key>', methods=['GET'])
-def vmmsgoptions(id, context, msg_key):
+@app.route("/vmmsgoption/<id>/<list>/<time>/<msg_key>", methods=['GET'])
+def vmmsgoptions(id, list, time, msg_key):
     action = request.args.get('Digits', '')
 
     response = plivo.Response()
@@ -234,42 +265,46 @@ def vmmsgoptions(id, context, msg_key):
 
     if action == '2':
         # Just play this message again
-        play_message(response, mailbox, context, msg)
+        play_message(response, mailbox, list, time, msg)
     elif action == '3' or action == '6' or action == '7':
         if action == '3':
             mailbox.delete_message(msg)
-            response.addSpeak(body='Message deleted')
+            response.addSpeak(body="Message deleted")
         elif action == '6':
-            response.addSpeak(body='Skip to next message')
+            response.addSpeak(body="Skip to next message")
         elif action == '7':
-            response.addSpeak(body='Message saved')
+            response.addSpeak(body="Message saved")
 
         # Find the next message to play
         next_msg = None
-        if context.startswith('n'):
+        if list == 'new':
             # Reviewing new messages - are there any more?
             if len(mailbox.new_msgs) > 0:
                 next_msg = mailbox.new_msgs[0]
             else:
-                # No more new messages, so move to the saved messages
-                context = 's' + context[1:]
-                unplayed_msgs = [x for x in mailbox.saved_msgs if x.received < int(context[1:])]
+                # No more new messages, so move to the saved messages.  Use the
+                # time passed in the request to filter any messages that have
+                # already been played as a new message.
+                list = 'saved'
+                unplayed_msgs = [x for x in mailbox.saved_msgs if x.received < int(time)]
                 if len(unplayed_msgs) > 0:
                     # There are messages in the saved list that haven't been played yet
-                    response.addSpeak(body='%d saved messages' % len(unplayed_msgs))
+                    response.addSpeak(body="%d saved messages" % len(unplayed_msgs))
                     next_msg = unplayed_msgs[0]
-        elif context.startswith('s'):
-            # Reviewing saved messages, have we reached the end.
-            unplayed_msgs = [x for x in mailbox.saved_msgs if x.received < int(context[1:])]
+        elif list == 'saved':
+            # Reviewing saved messages, have we reached the end.  Use the time
+            # passed in the request to filter any messages that have already
+            # been played as a new message.
+            unplayed_msgs = [x for x in mailbox.saved_msgs if x.received < int(time)]
             for unplayed_msg in unplayed_msgs:
                 if unplayed_msg.msg_key > msg_key:
                     next_msg = unplayed_msg
                     break
 
         if next_msg is not None:
-            play_message(response, mailbox, context, next_msg)
+            play_message(response, mailbox, list, time, next_msg)
         else:
-            response.addSpeak(body='No more messages')
+            response.addSpeak(body="No more messages")
             play_menu(response, mailbox)
 
     elif action == '1':
@@ -280,7 +315,7 @@ def vmmsgoptions(id, context, msg_key):
     xml_response.headers["Content-type"] = "text/xml"
     return xml_response
 
-@app.route('/vmmenu/<id>', methods=['GET'])
+@app.route("/vmmenu/<id>", methods=['GET'])
 def vmmenu(id):
     option = request.args.get('Digits', '')
 
@@ -289,33 +324,32 @@ def vmmenu(id):
 
     if option == '2':
         if len(mailbox.new_msgs) > 0:
-            # Start playing new messages.  Set context to the oldest new
-            # message.
-            response.addSpeak(body='%d new messages' % (len(mailbox.new_msgs)))
+            # Start playing new messages.  Pass the time of oldest new
+            # message so we spot new messages when they get moved to the saved
+            # list.
+            response.addSpeak(body="%d new messages" % (len(mailbox.new_msgs)))
             msg = mailbox.new_msgs[0]
-            context = 'n' + str(msg.received)
-            play_message(response, mailbox, context, msg)
+            play_message(response, mailbox, 'new', str(msg.received), msg)
         elif len(mailbox.saved_msgs) > 0:
             # Start playing saved messages.  Since there are no new messages
-            # set context to current time.
-            response.addSpeak(body='%d saved messages' % len(mailbox.saved_msgs))
+            # use the current time as the time of the first new message.
+            response.addSpeak(body="%d saved messages" % len(mailbox.saved_msgs))
             msg = mailbox.saved_msgs[0]
-            context = 's' + str(int(time.time()))
-            play_message(response, mailbox, context, msg)
+            play_message(response, mailbox, 'saved', str(int(time.time())), msg)
         else:
-            response.addSpeak(body='You have no messages.')
+            response.addSpeak(body="You have no messages.")
             play_menu(response, mailbox)
     elif option == '3':
-        response.addSpeak(body='Record your personal greeting after the beep.  Press # to end recording.')
+        response.addSpeak(body="Record your personal greeting after the beep.  Press # to end recording.")
         response.addRecord(
-          action='http://' + SERVER_NAME + '/vmgreeting/' + mailbox.id(),
+          action="http://" + SERVER_NAME + "/vmgreeting/" + mailbox.id(),
           fileFormat=RECORD_FORMAT,
           finishOnKey='#',
           method='GET')
-        response.addSpeak(body='Message not received')
+        response.addSpeak(body="Greeting not recorded")
         play_menu(response, mailbox)
     else:
-        response.addSpeak(body='Unknown selection')
+        response.addSpeak(body="Unknown selection")
         play_menu(response, mailbox)
 
     print response.to_xml()
@@ -323,7 +357,7 @@ def vmmenu(id):
     xml_response.headers["Content-type"] = "text/xml"
     return xml_response
 
-@app.route('/vmgreeting/<id>', methods=['GET'])
+@app.route("/vmgreeting/<id>", methods=['GET'])
 def vmgreeting(id):
     rec_file = request.args.get('RecordFile', '')
 
@@ -333,10 +367,10 @@ def vmgreeting(id):
     if rec_file != '':
         # Copy the greeting to the mailbox directory.
         mailbox.set_greeting(rec_file)
-        response.addSpeak(body='Your personal greeting has been updated. Thank you')
+        response.addSpeak(body="Your personal greeting has been updated. Thank you")
         play_menu(response, mailbox)
     else:
-        response.addSpeak(body='Greeting not updated.')
+        response.addSpeak(body="Greeting not updated.")
         play_menu(response, mailbox)
 
     print response.to_xml()
